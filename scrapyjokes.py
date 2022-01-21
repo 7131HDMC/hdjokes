2cda034708d960b73ef71547c415de3b7d75d9c9
Hari Dasa Fiuza de Moura <32579384+7131HDMC@users.noreply.github.com>
2022-01-21 12:50:54 -0300

    Update scrapyjokes.py

--- scrapyjokes.py
+++ scrapyjokes.py
@@ -9,15 +9,6 @@ class JokesSpider(scrapy.Spider):
 		'joke' 		: response.xpath("*//p[@class='piada']/text()").get(),
 		'category'  : response.xpath("*//h1/text()").get()
 		}
-
-		# quotes = response.xpath('*//div[@class="quote"]')
-		# for q in quotes:
-		# 	yield {
-		# 	'title': q.xpath(".//span[@class='text']/text()").get(),
-		# 	'author': q.xpath('.//small[@class="author"]/text()').get(),
-		# 	'tags': q.xpath(".//div[@class='tags']/a[@class='tag']/text()").getall()
-		# 	}
-
 		next_page = response.xpath("*//td/div[@class='seg']/a/@href").get()
 
 		if next_page is not None:
