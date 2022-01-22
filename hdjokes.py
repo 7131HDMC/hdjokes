946243d28b3e6028d2cc32663f5d6d961ee889c9
Hari Dasa Fiuza de Moura <haridasafiuza@gmail.com>
2022-01-22 13:08:51 -0300

    adjust manula jokes and

--- hdjokes.py
+++ hdjokes.py
@@ -5,18 +5,29 @@ import json
 # jokesdict = open('jokes.js#on')
 # 
 # 
+def head(listarr):
+	aux='Array is empty'
+	if len(listarr)>0:
+		print(listarr)
+		aux=listarr[0] 
+	return aux 
 
 class haridadejokes:
-	def __init__(self,language='pt'):
+	def __init__(self,language='pt',web=True):
 		#return dict
-		name = "jokes/"+language+"/jokes.json"
+		name = "jokes/"+language+"/jokes"	 
+		if web:
+			 name+="web"
+		name+=".json"  
+		#
 		print(name)
-		pt = json.load(open(name))
-		self.jokes = pt[language]
+		self.jokes = json.load(open(name))
 
 	#get jokes, futhermore will implemente list of ob to mixe categories and so on  	
 	def get_joke(self,category=''):
-		return random.choice(self.jokes)
+		select = random.choice(self.jokes)
+
+		return select['joke']
 
 
 
