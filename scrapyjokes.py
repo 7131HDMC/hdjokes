bd729f713c5fafd95910038b1248c355fd5f2545
Hari Dasa Fiuza de Moura <haridasafiuza@gmail.com>
2022-02-17 23:01:29 -0300

    multiple categories

--- scrapyjokes.py
+++ scrapyjokes.py
@@ -1,32 +1,44 @@
 import scrapy
+from scrapy.http.request import Request
 
 class JokesSpider(scrapy.Spider):
 	name = "JokesSpider"
 	start_urls = ['https://www.piadasnet.com/']
 	categoriesname = []
-
+	jokes=[]
+	categories=[]
 	def parse(self, response):
 		categoriesname = response.xpath("*//td[@class='menuEsq']/p[not(@class)]/a/text()").getall()
 
 		categoriesurls = response.xpath("*//td[@class='menuEsq']/p[not(@class)]/a/@href").getall()
 
+		print(categoriesurls)
+		print(categoriesname)
 
 		for idx, category in enumerate(categoriesname):
-			next_page = categoriesurls[category]
+			next_page = categoriesurls[idx]
+			# print('next_page = '+next_page)
+
 
 			if category is not None and next_page is not None:
-				yield {
-					category 		:  scrapy.Request(response.urljoin(next_page), callback=self.parsecategory),
-				# 'category'  : response.xpath("*//h1/text()").get()
-				}
+				yield Request(response.urljoin(next_page), meta={'category':category},callback=self.parsecategory)
+				# yield {
+				# 	category 		:  ,
+				# # 'category'  : response.xpath("*//h1/text()").get()
+				# }
+		# 		self.categories[category]=self.jokes
+		# 		self.jokes=[]
+		# yield self.categories		
 
 
 	def parsecategory(self, response):
+		category=response.meta['category']
+		# self.jokes.append(response.xpath("*//p[@class='piada']/text()").get())
 		yield {
-		'joke' 		: response.xpath("*//p[@class='piada']/text()").get(),
-		# 'category'  : response.xpath("*//h1/text()").get()
+			'joke' 		:  response.xpath("*//p[@class='piada']/text()").get(),
+			'category'  : category
 		}
 		next_page = response.xpath("*//td/div[@class='seg']/a/@href").get()
 
 		if next_page is not None:
-			yield scrapy.Request(response.urljoin(next_page), callback=self.parsecategory)
+			yield scrapy.Request(response.urljoin(next_page), meta={'category':category}, callback=self.parsecategory)
